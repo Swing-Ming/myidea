3a3a3d703d84cc23d887fdc269cb3d75a1bbfbfb
Swing-Wing <1026312846@qq.com>
2016-06-17 13:30:54 +0800

    更改了idea的位置

---
+++ idea/src/com/company/Junit_Test.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import org.junit.Assert;
+public class Junit_Test {
+    public void test(){
+        int i =3;
+        Assert.assertNotNull(i);
+    }
+}
---
+++ idea/src/com/company/NewTest.java
@@ -0,0 +1,43 @@
+package com.company;
+
+/**
+ * Created by Administrator on 2016/6/8.
+ */
+public class NewTest {
+
+    static int b = 1;
+    int d=0 ;
+    int c = 1;
+
+    private int s ;
+    private String a;
+    private String name ;
+    public void run(){
+
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
