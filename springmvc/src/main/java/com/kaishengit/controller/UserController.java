419dd4f48f52660c5d7c936dfdafa73714a26022
Swing-Wing <1026312846@qq.com>
2016-07-05 19:37:28 +0800

    ssm的使用

--- springmvc/src/main/java/com/kaishengit/controller/UserController.java
+++ springmvc/src/main/java/com/kaishengit/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.kaishengit.controller;
 
 import com.kaishengit.pojo.User;
+import com.sun.net.httpserver.HttpServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -43,7 +47,63 @@ public class UserController {
         logger.debug("userName{},Address{},Age{},comment{}",user.getUserName(),user.getAddress(),user.getAge(),comment);
         return "users/user";
     }
-    
+    @RequestMapping(value = "/{id:\\d+}/del",method = RequestMethod.GET)
+    public String del(@PathVariable Integer id){
+        logger.debug("del object{}",id);
+        return "redirect:/users";
+    }
+
+    //----------Ajax
+
+
+    @RequestMapping(value="/checkuser.json",
+                method = RequestMethod.GET,
+            produces = "text/html;charset=UTF-8")
+    @ResponseBody
+    public String checkUser(@PathVariable Integer id ){
+        logger.debug("ID{}",id);
+        return "success";
+    }
+
+    @RequestMapping(value="/{id},json",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public User showUser(){
+        User user = new User();
+        user.setAddress("中国河南");
+        user.setAge("23");
+        user.setUserName("张三");
+        return user;
+    }
+
+    //-----------native Servlet
+    @RequestMapping(value = "/native")
+    public String nativeHttpServlet(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    HttpSession session){
+
+        ServletContext context = session.getServletContext();
+        session.setAttribute("user","tom");
+        return "home";
+
+    }
+
+    //-----------upload
+
+    @RequestMapping(value="/avatar/upload",method=RequestMethod.GET)
+    public String uploadavatar(){
+        return "user/upload";
+    }
+
+   /* public String update(){
+        return
+    }*/
+
+
+
+
+
 
 
 
